642d476847ee5067a68a39bfe90d7bcc7575e906
eilmadc <57563030+eilmadc@users.noreply.github.com>
2023-03-30 09:57:11 +0200

    ejercicio3 + ii

--- ejercicio3.sql
+++ ejercicio3.sql
@@ -1,54 +1,28 @@
+/* Eliminación de la db si existe */
 DROP DATABASE IF EXISTS ejercicio3;
+
+/* Creacion de la db */
 CREATE DATABASE ejercicio3;
 
+/* Selección de la base de datos para comenzar a utilizar */
 USE ejercicio3;
 
-/* Entidad Comunidad Autonoma */
-CREATE TABLE  comunidad_autonoma(
-nombre_comunidad char(50) PRIMARY KEY,
-poblacion int,
-superficie int
+/*  Creación tabla --> Entidad Fabricantes */
+CREATE TABLE  fabricantes(
+	Codigo INT AUTO_INCREMENT PRIMARY KEY,
+	Nombre CHAR(100)
 );
 
-/* Entidad Provincia */
-CREATE TABLE provincia (
-cod_provincia int PRIMARY KEY,
-nombre_provincia char(50),
-poblacion int,
-superficie int,
-nombre_comunidad char(50),
-FOREIGN KEY (nombre_comunidad) REFERENCES comunidad_autonoma (nombre_comunidad)
-ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-/* Entidad Localidad */
-CREATE TABLE  localidad(
-nombre_localidad char(50) PRIMARY KEY,
-poblacion int,
-cod_provincia int NOT NULL,
-FOREIGN KEY (cod_provincia) REFERENCES provincia(cod_provincia)
-ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-/* Entidad Capital de Comunidad Autonoma */
-CREATE TABLE capital_ca (
-nombre_localidad char(50),
-KEY (nombre_localidad),
-nombre_comunidad char(50),
-KEY (nombre_comunidad),
-FOREIGN KEY (nombre_localidad) REFERENCES localidad (nombre_localidad),
-FOREIGN KEY (nombre_comunidad) REFERENCES comunidad_autonoma (nombre_comunidad)
-ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-/* Entidad Capital de provincia */
-CREATE TABLE capital_provincia (
-nombre_localidad char(50),
-KEY (nombre_localidad),
-cod_provincia int,
-KEY (cod_provincia),
-FOREIGN KEY (nombre_localidad) REFERENCES localidad (nombre_localidad),
-FOREIGN KEY (cod_provincia) REFERENCES provincia(cod_provincia)
-ON DELETE CASCADE ON UPDATE CASCADE
-);
-
+/* Creación tabla --> Entidad Artículos */
+CREATE TABLE articulos (
+Codigo INT AUTO_INCREMENT PRIMARY KEY,
+Nombre char(100),
+Precio INT,
+Fabricante INT  NOT NULL,
+FOREIGN KEY (Fabricante) REFERENCES fabricantes(Codigo) 
+ON DELETE CASCADE
+ON UPDATE CASCADE)
+ENGINE=InnoDB;
+;
+/* Mostrar tablas */
+SHOW TABLES;
